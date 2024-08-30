e594995d0adb686efbbe8d8f5de5e8b6ea2d0685
samir.xasiyev <samirakh@code.edu.az>
2024-08-31 02:34:08 +0400

    update task4.1 code

--- Task_4/Program.cs
+++ Task_4/Program.cs
@@ -1,9 +1,57 @@
-﻿namespace Task_4;
+﻿using System.Linq.Expressions;
+
+namespace Task_4;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+
+
+        /*
+         
+         Ashagidakilari C#-da yazin
+
+2. Sozlerden ibaret arrayde uzunlugu 4-den boyuk olan sozleri ekrana çap edin
+3. Ədədlərdən ibarət arrayda neçə rəqəm olduğunu ekrana çap edin (Məs: {1,77,6,14} arrayında 2 rəqəm var )
+4. Verilmish sozde 'a' herfi olub olmadigini tapan alqoritm
+
+5.(optional) Yalniz 1 for dongusu ile siralanmamish arrayi siralamaga calishin(qiymete tesir etmir)
+
+Ashagidaki movzulari arashdirin(Worst case-leri ve ishleme mentiqlerini):
+Mövzular:
+  Searching: Linear(Gostermishem)  , Binary(Gostermishem)  , Jump, Fibonacci, Exponential, Interpolation
+  Sorting: Bubble(Gostermishem), Insertion, Selection, Merge, Quick,  Heap
+         
+         
+         
+         */
+
+
+
+
+        // 1.Ədədlərdən ibarət arrayın ən kiçik elementini tapın
+
+        //int[] arry = { 54, 67, 99, 5, 67, 12, 43, 3,99 };
+
+        //int i = 1, temp = arry[0];
+        //for (; i < arry.Length; i++)
+        //{
+        //    if (arry[i]<temp)
+        //     temp = arry[i];
+            
+        //}
+        //Console.WriteLine($"Array de en kicik element :{temp}");
+
+
+
+
+
+
+
+
+
+
+
     }
 }
